a41d91b6bd0e9becc35a91a3a27e9849f6c335c6
Masen Matthews <masenmatthews@gmail.com>
2018-03-23 12:22:42 -0700

    build out initial JS functionality, troubleshoot API calls

---
+++ js/request.js
@@ -0,0 +1,17 @@
+export class Request {
+  constructor(name, issue) {
+  this.name = name;
+  this.issue = issue;
+}
+
+  findByName(){
+
+
+  }
+
+
+  findByIssue(){
+
+
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { Input } from './../js/input.js';
+import { Request } from './../js/request.js';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function() {
+
+  $("form#name-input").submit(function(event) {
+
+
+  });
+  $("form#issue-input").submit(function(event) {
+
+
+  });
+});
