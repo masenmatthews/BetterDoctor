64aa6a52804731ab714b27e33b06221f6a36ce1c
Masen Matthews <masenmatthews@gmail.com>
2018-03-23 12:50:56 -0700

    expand backend js

--- js/request.js
+++ js/request.js
@@ -4,14 +4,18 @@ export class Request {
   this.issue = issue;
 }
 
-  findByName(){
-
-
+  findByName(name){
+    let name = this.name;
+    const key = process.exports.apiKey
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&user_location=${location}&skip=0&limit=10&user_key=${apiKey}`).then(response => {showResults(results)
+    }).fail(() => 'Looks like something went wrong! Please try again at a later time.')
   }
 
 
-  findByIssue(){
-
-
+  findByIssue(issue){
+    let issue = this.issue;
+    const key = process.exports.apiKey
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&user_location=${location}&skip=0&limit=10&user_key=${apiKey}`).then(response => {showResults(results)
+    }).fail(() => 'Looks like something went wrong! Please try again at a later time.')
   }
 }
