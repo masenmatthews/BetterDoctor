eb8fe7a8686cfa12304e8190da7d2bcb4a15a080
Masen Matthews <masenmatthews@gmail.com>
2018-03-23 12:54:00 -0700

    troubleshoot API key

--- js/request.js
+++ js/request.js
@@ -7,7 +7,7 @@ export class Request {
   findByName(name){
     let name = this.name;
     const key = process.exports.apiKey
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&user_location=${location}&skip=0&limit=10&user_key=${apiKey}`).then(response => {showResults(results)
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&user_location=${location}&skip=0&limit=10&user_key=${apiKey}`).then(response => {displayResult()
     }).fail(() => 'Looks like something went wrong! Please try again at a later time.')
   }
 
@@ -15,7 +15,7 @@ export class Request {
   findByIssue(issue){
     let issue = this.issue;
     const key = process.exports.apiKey
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&user_location=${location}&skip=0&limit=10&user_key=${apiKey}`).then(response => {showResults(results)
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&user_location=${location}&skip=0&limit=10&user_key=${apiKey}`).then(response => {displayResult()
     }).fail(() => 'Looks like something went wrong! Please try again at a later time.')
   }
 }
