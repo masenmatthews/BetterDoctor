1c2c157cf4ffb9f84d5c2c41a4d72ae55817b497
Masen Matthews <masenmatthews@gmail.com>
2018-03-23 13:36:15 -0700

    attempt to establish front end js functionality

--- src/main.js
+++ src/main.js
@@ -8,11 +8,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 $(document).ready(function() {
 
   $("form#name-input").submit(function(event) {
-
+    let name = $('#name').val();
+    $('#name').val("");
+    success: function(response) {
 
   });
   $("form#issue-input").submit(function(event) {
-
+    let issue = $('#issue').val();
+    $('#issue').val("");
 
   });
 });
