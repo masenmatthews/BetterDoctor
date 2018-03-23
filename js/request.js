858d2be47cb611e81da99815c73acc08fb8aa86c
Masen Matthews <masenmatthews@gmail.com>
2018-03-23 15:19:32 -0700

    resolve errors, API data still not populating page

--- js/request.js
+++ js/request.js
@@ -1,20 +1,20 @@
-export class Request {
-  constructor(name, issue) {
-  this.name = name;
-  this.issue = issue;
-}
-
-  findByName(){
-    let name = this.name;
-    const key = process.exports.apiKey
-
-  }
-
-
-  findByIssue(){
-    let issue = this.issue;
-    const key = process.exports.apiKey
-
-
-  }
-}
+// export class Request {
+//   constructor(name, issue) {
+//   this.name = name;
+//   this.issue = issue;
+// }
+//
+//   findByName(){
+//     let name = this.name;
+//     const key = process.exports.apiKey
+//
+//   }
+//
+//
+//   findByIssue(){
+//     let issue = this.issue;
+//     const key = process.exports.apiKey
+//
+//
+//   }
+// }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import { Request } from './../js/request.js';
+// import { Request } from './../js/request.js';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
-  $("#name-input").click(function(event) {
-    let name = $('#name').val();
+  $("#name-input").click(function() {
+    let test = $('#name').val();
     $('#name').val("");
     $.ajax({
-    url: `https://api.betterdoctor.com/2016-03-01/doctors?first_name=Mary&last_name=Smith&location=or-portland&user_location=45.5231%2C%20122.6765&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`,
+    url: `https://api.betterdoctor.com/2016-03-01/doctors?name=${test}&location=or-portland&user_location=45.5231%2C%20122.6765&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`,
     type: 'GET',
     data: {
       format: 'json'
