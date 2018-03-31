e18189c587e86de1d88da126273dd34a023ac389
Masen Matthews <masenmatthews@gmail.com>
2018-03-31 14:12:41 -0700

    test

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // $(document).ready(function() {
    $("#name-submit").submit(function(event) {
-     event.preventDefault();
+    event.preventDefault();
     let name = $('#name').val();
     $.ajax({
     url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`,
