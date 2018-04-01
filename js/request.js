1b7600925106261ac50603f4cbfb837a101bb32e
Masen Matthews <masenmatthews@gmail.com>
2018-03-31 20:19:12 -0700

    refactor business vs. user logic

--- js/request.js
+++ js/request.js
@@ -1,20 +1,20 @@
-// export class Request {
-//   constructor(name, issue) {
-//   this.name = name;
-//   this.issue = issue;
-// }
-//
-//   findByName(){
-//     let name = this.name;
-//     const apiKey = process.exports.apiKey
-//
-//   }
-//
-//
-//   findByIssue(){
-//     let issue = this.issue;
-//     const apiKey = process.exports.apiKey
-//
-//
-//   }
-// }
+export class Request {
+  doctorLookup(name, searchRequest) {
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`)
+  .then(function(response) {
+    if(response.data.length === 0 {
+      $('#doctorName').text('There are no results that fit your criteria. Please try again.')
+    } else {
+      searchRequest(response);
+    }
+  }
+}
+
+  symptomLookup(symptom, searchRequest) { $.get(`https://api.betterdoctor.com/2016-03-01/specialties?query=${symptom}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`)    .then(function(response) {
+    if(response.data.length === 0 {
+      $('#doctorName').text('There are no results that fit your criteria. Please try again.')
+    } else {
+      searchRequest(response);
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
-// import { Request } from './../js/request.js';
+import { Request } from './../js/request.js';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// $(document).ready(function() {
-   $("#name-submit").submit(function(event) {
+document.ready(function() {
+  let request = new Request;
+
+  $("#name-submit").submit(function(event) {
     event.preventDefault();
+    request.doctorLookup(name, searchRequest);
     let name = $('#name').val();
-    $.ajax({
-    url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`,
-    type: 'GET',
-    data: {
-      format: 'json'
-    },
     success: function(response) {
       for (let i = 0; i < response.data.length; i++) {
         let doctorName = response.data[i].profile.first_name + ' ' + response.data[i].profile.last_name;
@@ -32,5 +29,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
       $('#errors').text("Looks like something went wrong! Please try again later.")
     }
     });
-  });
-// });
+
+  $("#symptom-submit").submit(function(event) {
+   event.preventDefault();
+   request.symptomLookup(symptom, searchRequest);
+   let symptom = $('#symptom').val();
+   success: function(response) {
+     for (let i = 0; i < response.data.length; i++) {
+       let doctorName = response.data[i].profile.first_name + ' ' + response.data[i].profile.last_name;
+       let addressLineOne = response.data[i].practices[0].visit_address.street;
+       let addressLineTwo = response.data[i].practices[0].visit_address.city + " " + response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip;
+       let phoneNumber = response.data[i].practices[0].phones[0].number;
+       let doctorWebsite = response.data[i].practices[0].website;
+       let description = response.data[i].description[0];
+       let practiceName = response.data[i].name[0];
+       $('#descriptions').text(`${description}`);
+       $('#practice-name').text(`${practiceName}`);
+       $('#doctorName').text(`${doctorName}`);
+       $('#addressOne').text(`${addressLineOne}`);
+       $('#addressTwo').text(`${addressLineTwo}`);
+       $('#phone').text(`${phoneNumber}`);
+       $('#website').text(`${doctorWebsite}`);
+     }
+   },
+   error: function() {
+     $('#errors').text("Looks like something went wrong! Please try again later.")
+   }
+   });
+}
