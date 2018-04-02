eb704546524118243e3f771442ab907f51a9dd86
Masen Matthews <masenmatthews@gmail.com>
2018-04-01 18:21:56 -0700

    refactor error messages

--- js/request.js
+++ js/request.js
@@ -3,20 +3,24 @@ import $ from 'jquery';
 export class Request {
   doctorLookup(name, doctorLookup) {
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`).then(response => {
-    if(response.data.length === 0) {
-      $('#doctorName').text('There are no results that fit your criteria. Please try again.')
+      if (response.data.profile.first_name === undefined) {
+      $('#errors').text('Looks like there was a problem retrieving this information Please try again later.')
+    } else if (response.data.length === 0) {
+      $('#errors').text('Your search criteria did not return any results. Please try using a different entry.')
     } else {
       doctorLookup(response);
     }
   })
 }
 
-  symptomLookup(symptom, symptomLookup) { $.get(`https://api.betterdoctor.com/2016-03-01/specialties?query=${symptom}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`).then(response => {
-    if(response.data.length === 0) {
-      $('#doctorName').text('There are no results that fit your criteria. Please try again.')
+    symptomLookup(symptom, symptomLookup) { $.get(`https://api.betterdoctor.com/2016-03-01/specialties?query=${symptom}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`).then(response => {
+      if (response.data.profile.first_name === undefined) {
+      $('#errors').text('Looks like there was a problem retrieving this information Please try again later.')
+    } else if (response.data.length === 0) {
+      $('#errors').text('Your search criteria did not return any results. Please try using a different entry.')
     } else {
-      symptomLookup(response);
-    }
-  })
-}
+        symptomLookup(response);
+      }
+    })
+  }
 }
--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-    // let request = new Request();
-    // request.doctorLookup(name, success);
     let doctorLookupResult = response => {
       for (let i = 0; i < response.data.length; i++) {
         let doctorName = response.data[i].profile.first_name + ' ' + response.data[i].profile.last_name;
@@ -21,11 +19,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
       }
     }
 
-    // error: function() {
-    //   $('#errors').text("Looks like something went wrong! Please try again later.")
-    // }
-
-   // request.symptomLookup(symptom, success);
     let symptomLookupResult = response => {
      for (let i = 0; i < response.data.length; i++) {
        let doctorName = response.data[i].profile.first_name + ' ' + response.data[i].profile.last_name;
@@ -42,28 +35,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
        $('#addressTwo').text(`${addressLineTwo}`);
        $('#phone').text(`${phoneNumber}`);
        $('#website').text(`${doctorWebsite}`);
-       }
      }
-
-
-   // },
-   // error: function() {
-   //   $('#errors').text("Looks like something went wrong! Please try again later.")
-   // }
-   // });
-
+   }
 
    $(document).ready(() => {
      let request = new Request;
      $("#name-submit").submit(function(event) {
-     let name = $('#name').val();
-     event.preventDefault();
-     request.doctorLookup(name, doctorLookupResult)
-   })
+       let name = $('#name').val();
+       event.preventDefault();
+       request.doctorLookup(name, doctorLookupResult)
+     })
      $("#symptom-submit").submit(function(event) {
-     let symptom = $('#symptom').val();
-     event.preventDefault();
-     request.symptomLookup(symptom, symptomLookupResult);
-   })
-
-});
+       let symptom = $('#symptom').val();
+       event.preventDefault();
+       request.symptomLookup(symptom, symptomLookupResult);
+     })
+  });
