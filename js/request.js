e122b200aed3e8f8527a0194a0aed4e57a0a5954
Masen Matthews <masenmatthews@gmail.com>
2018-04-03 15:00:55 -0700

    remove harcoding for API key

--- js/request.js
+++ js/request.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 export class Request {
   doctorLookup(name, doctorLookup) {
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`).then(response => {
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=${process.env.exports.apiKey}`).then(response => {
     //   if (response.data.profile.first_name === undefined) {
     //   $('#errors').text('Looks like there was a problem retrieving this information Please try again later.')
     // } else
@@ -14,7 +14,7 @@ export class Request {
   })
 }
 
-    symptomLookup(symptom, symptomLookup) { $.get(`https://api.betterdoctor.com/2016-03-01/specialties?query=${symptom}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=efae18ff8264be586e0528ee22d4acec`).then(response => {
+    symptomLookup(symptom, symptomLookup) { $.get(`https://api.betterdoctor.com/2016-03-01/specialties?query=${symptom}&location=45.5231%2C-122.6756%2C10&user_location=45.5231%2C-122.6756&skip=0&limit=10&user_key=${process.env.exports.apiKey}`).then(response => {
     //   if (response.data.profile.first_name === undefined) {
     //   $('#errors').text('Looks like there was a problem retrieving this information Please try again later.')
     // } else
